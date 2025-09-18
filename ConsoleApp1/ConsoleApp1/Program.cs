b053119fcdf6701b62a123a4947c059db3bb0bfe
Fernando Noguera <fernandonoguera95@outlook.com>
2025-09-17 20:10:17 -0600

    implementación de la validación de roles

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,33 +1,54 @@
-﻿using System;
-
-class Program()
+﻿namespace ConsoleApp1
 {
-    static void Main(string[] args)
+    class Program
     {
-        ShowMenu();
+        static void Main(string[] args)
+        {
+            ShowMenu();
+        }
+
+        #region Show Main Menu
+        static void ShowMenu()
+        {
+            string employeeId = string.Empty;
+            string employeeName = string.Empty;
+            byte employeeRole = 0;
+
+            Console.WriteLine("Identificación del empleado: ");
+            employeeId = Console.ReadLine();
+
+            Console.WriteLine("\nNombre del empleado: ");
+            employeeName = Console.ReadLine();
+
+            Console.WriteLine("\nRol del empleado: ");
+            Console.WriteLine("1 = Operario \n2 = Técnico \n3 = Profesional");
+            employeeRole = byte.Parse(Console.ReadLine());
+
+            Console.Clear();
+            RoleValidation(employeeRole);
+        }
+        #endregion
+
+        #region Employee Role Validation
+        //the function to verify the employee's role has to be created
+        static void RoleValidation(byte employeeRole)
+        {
+            switch (employeeRole)
+            {
+                case 1:
+                    Console.WriteLine("Se ha seleccionado el rol: Operario");
+                    break;
+
+                case 2:
+                    Console.WriteLine("Se ha seleccionado el rol: Técnico");
+                    break;
+
+                case 3:
+                    Console.WriteLine("Se ha seleccionado el rol: Profesional");
+                    break;
+            }
+        }
+
+        #endregion
     }
-
-    #region Show Main Menu
-    static void ShowMenu()
-    {
-        string employeeId = string.Empty;
-        string employeeName = string.Empty;
-        byte employeeRole = 0;
-
-        Console.WriteLine("Identificación del empleado: ");
-        employeeId = Console.ReadLine();
-
-        Console.WriteLine("\nNombre del empleado: ");
-        employeeName = Console.ReadLine();
-
-        Console.WriteLine("\nRol del empleado: ");
-        Console.WriteLine("1 = Operario \n2 = Técnico \n3 = Profesional");
-        employeeRole = byte.Parse(Console.ReadLine());
-    }
-    #endregion
-
-    #region Employee Role Validation
-    //the function to verify the employee's role has to be created
-
-    #endregion
 }
