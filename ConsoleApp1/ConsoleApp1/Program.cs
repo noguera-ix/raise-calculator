e531d143b8251ad650823f7366d6a3ff6d6ca185
Fernando Noguera <fernandonoguera95@outlook.com>
2025-09-17 18:29:45 -0600

    implementación del menú principal

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program()
+{
+    static void Main(string[] args)
+    {
+        ShowMenu();
+    }
+
+    #region Show Main Menu
+    static void ShowMenu()
+    {
+        string employeeId = string.Empty;
+        string employeeName = string.Empty;
+        byte employeeRole = 0;
+
+        Console.WriteLine("Identificación del empleado: ");
+        employeeId = Console.ReadLine();
+
+        Console.WriteLine("\nNombre del empleado: ");
+        employeeName = Console.ReadLine();
+
+        Console.WriteLine("\nRol del empleado: ");
+        Console.WriteLine("1 = Operario \n2 = Técnico \n3 = Profesional");
+        employeeRole = byte.Parse(Console.ReadLine());
+    }
+    #endregion
+
+    #region Employee Role Validation
+    //the function to verify the employee's role has to be created
+
+    #endregion
+}
