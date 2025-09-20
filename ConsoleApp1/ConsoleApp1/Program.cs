6d82cae02ad1f646b4226f865c343beaf0593e5b
Fernando Noguera <fernandonoguera95@outlook.com>
2025-09-19 20:14:43 -0600

    implementació de funciones para calcular salarios y aumentos en según puesto

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,6 +10,7 @@ namespace ConsoleApp1
         }
 
         #region Show Main Menu
+
         static void ShowMenu()
         {
             string employeeId = string.Empty;
@@ -29,9 +30,11 @@ namespace ConsoleApp1
             Console.Clear();
             RoleValidation(employeeRole);
         }
+
         #endregion
 
         #region Employee Role Validation
+
         static void RoleValidation(byte employeeRole)
         {
             switch (employeeRole)
@@ -43,25 +46,72 @@ namespace ConsoleApp1
 
                 case 2:
                     Console.WriteLine("Se ha seleccionado el rol: Técnico");
+                    TechnicianPayment();
                     break;
 
                 case 3:
                     Console.WriteLine("Se ha seleccionado el rol: Profesional");
+                    ProfessionalPayment();
                     break;
             }
         }
+
         #endregion
 
         #region Role Payment and Raise
-        
+
         // this function calculates the payment for an operator
         static void OperatorPayment()
         {
             byte workedHours = 0;
-            float raise = 0.15f;
-            float baseSalary = 0;
+            float raise = 1.15f;
+            float hourSalary = 0;
+            float grossSalary = 0;
 
+            Console.WriteLine("\nIngrese el salario del empleado: ");
+            hourSalary = float.Parse(Console.ReadLine()!);
 
+            Console.WriteLine("\nIngrese las horas laboradas: ");
+            workedHours = byte.Parse(Console.ReadLine()!);
+
+            grossSalary = (hourSalary * workedHours) * raise;
+            Console.WriteLine("\nSalario reportado: " + grossSalary.ToString());
+        }
+
+        // this function calculates the payment for a technician
+        static void TechnicianPayment()
+        {
+            byte workedHours = 0;
+            float raise = 1.10f;
+            float hourSalary = 0;
+            float grossSalary = 0;
+
+            Console.WriteLine("\nIngrese el salario del empleado: ");
+            hourSalary = float.Parse(Console.ReadLine()!);
+
+            Console.WriteLine("\nIngrese las horas laboradas: ");
+            workedHours = byte.Parse(Console.ReadLine()!);
+
+            grossSalary = (hourSalary * workedHours) * raise;
+            Console.WriteLine("\nSalario reportado: " + grossSalary.ToString());
+        }
+
+        //this function calculates the payment for a professional
+        static void ProfessionalPayment()
+        {
+            byte workedHours = 0;
+            float raise = 1.05f;
+            float hourSalary = 0;
+            float grossSalary = 0;
+
+            Console.WriteLine("\nIngrese el salario del empleado: ");
+            hourSalary = float.Parse(Console.ReadLine()!);
+
+            Console.WriteLine("\nIngrese las horas laboradas: ");
+            workedHours = byte.Parse(Console.ReadLine()!);
+
+            grossSalary = (hourSalary * workedHours) * raise;
+            Console.WriteLine("\nSalario reportado: " + grossSalary.ToString());
         }
 
         #endregion
